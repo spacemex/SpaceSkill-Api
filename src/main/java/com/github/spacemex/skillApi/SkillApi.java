87d7970af176c1bb266f1c2cc74963010e34b05e
deven <devenshona@gmail.com>
2024-10-17 14:38:13 -0500

    Debug

--- src/main/java/com/github/spacemex/skillApi/SkillApi.java
+++ src/main/java/com/github/spacemex/skillApi/SkillApi.java
@@ -2,6 +2,13 @@ package com.github.spacemex.skillApi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class SkillApi extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        Logger logger = getLogger();
+        logger.info("SkillAPI has been enabled!");
+    }
 }
-//DELETE THIS COMMENT
