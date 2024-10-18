8494944c38fdd665c915b6d53b9ce01b4e7d303f
deven <devenshona@gmail.com>
2024-10-18 17:54:21 -0500

    removed unneeded variable

--- src/main/java/com/github/spacemex/skillApi/SkillApi.java
+++ src/main/java/com/github/spacemex/skillApi/SkillApi.java
@@ -2,13 +2,10 @@ package com.github.spacemex.skillApi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public final class SkillApi extends JavaPlugin {
 
     @Override
     public void onEnable(){
-        Logger logger = getLogger();
-        logger.info("SkillAPI has been enabled!");
+        getLogger().info("SkillAPI has been enabled!");
     }
 }
